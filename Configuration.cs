6dc318fe1359c84fe171a75e2ff8e867767b3e0d
MayconRDO <41632489+MayconRDO@users.noreply.github.com>
2022-09-28 12:05:59 -0300

    Added the whole project

---
+++ Configuration.cs
@@ -0,0 +1,18 @@
+namespace Blog;
+
+public static class Configuration
+{
+    public static string JwtKey = "mGq3kwmISs6l/gAALLz8Qw";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+}
